efcc82c3b32c33ea60313b2fbfb5cbac0aa7391d
SmileyFace799 <Henrikgran85@gmail.com>
2024-01-24 17:32:33 +0100

    Added comments to the example code

--- src/main/java/currencyexchanger/MainApp.java
+++ src/main/java/currencyexchanger/MainApp.java
@@ -15,16 +15,20 @@ class MainApp {
      *             https://freecurrencyapi.com/</a> as the 1st argument
      */
     public static void main(String[] args) {
+        // Guard condition to verify that an API key was provided
         if (args.length < 1 || args[0] == null) {
             throw new IllegalArgumentException("An API key should be provided as the 1st argument");
         }
 
+        // Creates the RatesManager
         RatesManager ratesManager = new RatesManager.Builder(args[0])
-                .setBaseCurrency("NOK")
-                .setCurrencies("SEK", "DKK", "USD", "EUR")
+                .setBaseCurrency("NOK") // Sets the base currency
+                .setCurrencies("SEK", "DKK", "USD", "EUR") // Sets the currencies to retrieve exchange rates to
                 .build();
 
-        ratesManager.addListener(rates -> Logger.getLogger(MainApp.class.getName()).log(Level.INFO, "Received rates: {0}", rates));
-        ratesManager.start(30, TimeUnit.MINUTES);
+        // Creates a simple listener that logs any received rate updates, so they appear in the console
+        RatesUpdateListener logReceivedRates = rates -> Logger.getLogger(MainApp.class.getName()).log(Level.INFO, "Received rates: {0}", rates);
+        ratesManager.addListener(logReceivedRates); // Adds the listener
+        ratesManager.start(30, TimeUnit.MINUTES); // Starts the manager, & sets it to retrieve updated rates every 30min
     }
 }
